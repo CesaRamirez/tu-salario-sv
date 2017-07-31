a2f7ec74a85cb6d79b2d7635eb4b56612f2ac9f5
César Ramírez <cesar.ramirez.sv@gmail.com>
2017-07-31 10:32:20 -0600

    Add packages and setup initial config

--- config/app.php
+++ config/app.php
@@ -167,6 +167,8 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        Spatie\Fractal\FractalServiceProvider::class,
+        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
 
         /*
          * Application Service Providers...
