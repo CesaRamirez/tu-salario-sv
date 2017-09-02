f95f0fe487f9a3ba8c2e084e2c748419d2bacb23
César Ramírez <cesar.ramirez.sv@gmail.com>
2017-09-02 12:48:08 -0600

    Upgrade vuetify

--- resources/assets/js/app/Navigation.vue
+++ resources/assets/js/app/Navigation.vue
@@ -8,9 +8,6 @@
           absolute>
             <v-list class="pa-0">
                 <v-list-tile avatar tag="div">
-                  <v-list-tile-avatar>
-                    <img src="https://randomuser.me/api/portraits/men/85.jpg" />
-                  </v-list-tile-avatar>
                   <v-list-tile-content>
                     <v-list-tile-title>{{ user.data.name }}</v-list-tile-title>
                   </v-list-tile-content>
@@ -21,17 +18,17 @@
                   </v-list-tile-action>
                 </v-list-tile>
              </v-list>
-      <v-list class="pt-0" dense>
-        <v-divider></v-divider>
-        <v-list-tile v-for="item in items" :key="item.title" :to="{ name: item.to }">
-            <v-list-tile-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
+             <v-list class="pt-0" dense>
+                <v-divider></v-divider>
+                <v-list-tile v-for="item in items" :key="item.title" :to="{ name: item.to }" exact>
+                    <v-list-tile-action>
+                      <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                      <v-list-tile-title v-text="item.title"></v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
         </v-navigation-drawer>
         <v-toolbar dark>
             <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('layouts.app');
-});
+$router->view('/', 'app');
 
 $router->get('/{vue_capture?}', function () {
-    return view('layouts.app');
+    return view('app');
 })->where('vue_capture', '[\/\w\.-]*');
