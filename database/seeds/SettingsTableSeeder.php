dcbe7fd32e552c4fd1d8d2a77b3fb8b596afca06
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2017-11-09 09:35:38 -0600

    Refactor settings seeder

--- database/seeds/SettingsTableSeeder.php
+++ database/seeds/SettingsTableSeeder.php
@@ -9,34 +9,30 @@ class SettingsTableSeeder extends Seeder
      */
     public function run()
     {
-        $settings = [
-            [
-                'key'         => 'AFP',
-                'description' => 'Porcentaje de AFP',
-                'value'       => 6.25,
-            ],
-            [
-                'key'         => 'ISSS',
-                'description' => 'Porcentaje de Seguro Social',
-                'value'       => 3.0,
-            ],
-            [
-                'key'         => 'SALARIO_MINIMO',
-                'description' => 'Valor de Salario Minimo',
-                'value'       => 300.00,
-            ],
-            [
-                'key'         => 'VACACION',
-                'description' => 'Porcentaje de pago de Vacación',
-                'value'       => 30,
-            ],
-            [
-                'key'         => 'DIAS_VACACION',
-                'description' => 'Dias de pago de Vacación',
-                'value'       => 15,
-            ],
-        ];
+        DB::table('settings')->insert($this->setting('AFP', 'Porcentaje de AFP', 7.25));
+        DB::table('settings')->insert($this->setting('ISSS', 'Porcentaje de Seguro Social', 3.0));
+        DB::table('settings')->insert($this->setting('SALARIO_MINIMO', 'Valor de Salario Minimo', 300.00));
+        DB::table('settings')->insert($this->setting('VACACION', 'Porcentaje de pago de Vacación', 30));
+        DB::table('settings')->insert($this->setting('DIAS_VACACION', 'Dias de pago de Vacación', 15));
+    }
 
-        DB::table('settings')->insert($settings);
+    /**
+     * Set setting for seeder.
+     *
+     * @param string $key
+     * @param string $description
+     * @param float  $value
+     *
+     * @return array
+     */
+    protected function setting(string $key, string $description, float $value)
+    {
+        return [
+              'key'         => $key,
+              'description' => $description,
+              'value'       => $value,
+              'created_at'  => now(),
+              'updated_at'  => now(),
+        ];
     }
 }
