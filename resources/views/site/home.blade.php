b8f5b11972c67848c407c89e96ea9bcaf85bd492
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2017-11-20 10:18:09 -0600

    Improvements hover

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -7,14 +7,14 @@
 				<img src="{{ asset('storage/assets/svg/logo_white.svg') }}" alt="Logo" class="h-11 w-40">
 			</div>
 			<div class="justify-end">
-				<ul class="list-reset flex items-center text-white leading-loose text-base cl-effect">
+				<ul class="list-reset flex items-center leading-loose text-xl cl-effect">
 					<li class="mr-12 ">
                         <a href="" class="no-underline text-white border-b-3">Salario</a>
                     </li>
 					<li class="mr-12">
                         <a href="" class="no-underline text-white">Vacaciones</a>
                     </li>
-					<li class="cl-effect">
+					<li>
                         <a href="#" class="no-underline text-white">Aguinaldo</a>
                     </li>
 				</ul>
@@ -23,7 +23,7 @@
 		<div class="flex justify-center items-center flex-col h-full text-center">
 			<img src="{{ asset('storage/assets/svg/isotipo.svg') }}" alt="" class="h-12 w-12 mb-4">
 			<h1 class="text-white text-6xl font-medium mb-4">Calculá tu salario</h1>
-			<p class="text-white w-1/3 font-light text-xl roman mb-12 leading-normal">Con SalarioSV podés calcular fácilmente tu salario, vacaciones y aguinaldo.</p>
+			<p class="text-white lg:w-1/3 font-light text-xl roman mb-12 leading-normal">Con SalarioSV podés calcular fácilmente tu salario, vacaciones y aguinaldo.</p>
 			<a href="" class="no-underline py-2 px-8 font-bold rounded-full text-white bg-blue-dark mb-12 hover:bg-white hover:text-blue-dark border-blue-dark border-2">Comenzar</a>
 			<div id="scroll-down" class="mt-8 scroll">
 				<a href="#calculate" class="js-trigger">
@@ -31,24 +31,27 @@
 			</div>
 		</div>
 	</header>
-	<section class="bg-white h-1/2 flex flex-col items-center justify-center text-center border-b"
+	<section class="bg-white lg:h-1/2 py-8 flex flex-col items-center justify-center text-center border-b"
 	 id="calculate">
-		<h4 class="text-2xl text-grey-dark mb-6">Comenzá a calcular tu</h4>
-		<div class="flex flex-row justify-around w-3/4">
-			<div class="border rounded shadow-1 hvr-float-shadow">
-				<a href="#">
-					<img src="{{ asset('storage/assets/svg/icons/ic_salario.svg') }}" alt="" class="w-3/4">
+		<h4 class="text-3xl text-grey-dark mb-6">Comenzá a calcular tu</h4>
+		<div class="flex lg:flex-row md:flex-row flex-col justify-around w-3/4">
+			<div class="border rounded shadow-1 hvr-underline-from-left py-4">
+				<a href="#" class="no-underline">
+					<img src="{{ asset('storage/assets/svg/icons/ic_salario.svg') }}" alt="" class="w-3/4 -mb-6">
 				</a>
+                <h4 class="text-2xl font-normal text-blue">Salario</h4>
 			</div>
-			<div class="border rounded shadow-1 hvr-float-shadow">
+			<div class="border rounded shadow-1 hvr-underline-from-center py-4">
 				<a href="#">
-					<img src="{{ asset('storage/assets/svg/icons/ic_vacaciones.svg') }}" alt="" class="w-3/4">
+					<img src="{{ asset('storage/assets/svg/icons/ic_vacaciones.svg') }}" alt="" class="w-3/4 -mb-6">
 				</a>
+                <h4 class="text-2xl font-normal text-blue">Vacación</h4>
 			</div>
-			<div class="border rounded shadow-1 hvr-float-shadow">
+			<div class="border rounded shadow-1 hvr-underline-from-right py-4">
 				<a href="#">
-					<img src="{{ asset('storage/assets/svg/icons/ic_aguinaldo.svg') }}" alt="" class="w-3/4">
+					<img src="{{ asset('storage/assets/svg/icons/ic_aguinaldo.svg') }}" alt="" class="w-3/4 -mb-6">
 				</a>
+                <h4 class="text-2xl font-normal text-blue">Aguinaldo</h4>
 			</div>
 		</div>
 	</section>	
