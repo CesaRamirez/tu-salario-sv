23a1c34070637b9a0ee97658c5faf2a371c2d565
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2017-11-21 21:21:50 -0600

    Remove unnecesary value un webpack admin file

--- webpack.mix.admin.js
+++ webpack.mix.admin.js
@@ -3,5 +3,4 @@ require('dotenv').config();
 
 mix.js('resources/assets/js/admin/app.js', 'public/js/admin/')
 .extract(['vue', 'axios', 'vuetify', 'vue-router', 'vee-validate', 'lodash', 'collect.js'], 'js/admin/vendor.js')
-.sass('resources/assets/sass/admin/app.scss', 'public/css/admin/')
-.purifyCss(true);
+.sass('resources/assets/sass/admin/app.scss', 'public/css/admin/');
