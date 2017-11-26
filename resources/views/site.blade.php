917af3c668f5cd8bfd622807325d16ca095a7f34
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2017-11-26 13:43:27 -0600

    better SEO

--- resources/views/site.blade.php
+++ resources/views/site.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="description" content="Con SalarioSV podés calcular tu salario, vacaciones y aguinaldo de una manera fácil y rápida.">
    
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
@@ -14,10 +15,12 @@
     <meta name="application-name" content="SalarioSV">
     <meta name="theme-color" content="#2f80ed">
 
+    <link rel=”canonical” href=”http://www.salariosv.com” />
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }} - Calculá tu Salario, Vacación ó Aguinaldo</title>
 
     <!-- Styles -->
     <link href="{{ mix('css/site/app.css') }}" rel="stylesheet" type="text/css">
--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -1,10 +1,17 @@
 @extends('site') @section('content')
-<img src="{{ asset('storage/assets/svg/pattern.svg') }}" alt="" class="absolute bg-cover h-screen w-100" style="object-fit: cover">
+<img src="{{ asset('storage/assets/svg/pattern.svg') }}" 
+	 alt="Patrón de Imagotipo" 
+	 title="Patrón de Imagotipo" 
+	 class="absolute bg-cover h-screen w-screen" 
+	 style="object-fit: cover">
 <div class="h-screen">
 	<header class="relative container mx-auto h-screen">
 		<nav class="flex items-center lg:justify-between justify-center pt-10">
 			<div class="">
-				<img src="{{ asset('storage/assets/svg/logo_white.svg') }}" alt="Logo" class="h-11 w-40">
+				<img src="{{ asset('storage/assets/svg/logo_white.svg') }}" 
+					 alt="Imagotipo de SalarioSV" 
+					 title="Imagotipo de SalarioSV" 
+					 class="h-11 w-40">
 			</div>
 			<div class="justify-end hidden lg:inline">
 				<ul class="list-reset flex items-center leading-loose text-lg cl-effect">
@@ -21,13 +28,13 @@
 			</div>
 		</nav>
 		<div class="flex justify-center items-center flex-col h-full text-center">
-			<img src="{{ asset('storage/assets/svg/isotipo.svg') }}" alt="" class="h-12 w-12 mb-4 hidden lg:inline">
+			<img src="{{ asset('storage/assets/svg/isotipo.svg') }}" alt="Isotipo de SalarioSV" title="Isotipo de SalarioSV" class="h-12 w-12 mb-4 hidden lg:inline">
 			<h1 class="text-white lg:text-6xl text-4xl font-medium mb-4">Calculá tu salario</h1>
 			<p class="text-white lg:w-1/3 font-light lg:text-xl roman mb-12 leading-normal">Con SalarioSV podés calcular fácilmente tu salario, vacaciones y aguinaldo.</p>
-			{{--  <a href="" class="no-underline py-2 px-8 font-bold rounded-full text-white bg-blue-dark mb-12 hover:bg-white hover:text-blue-dark border-blue-dark border-2">Comenzar</a>  --}}
-			<div id="scroll-down" class="mt-8  scroll">
+			<div id="scroll-down" class="mt-12 scroll">
 				<a href="#calculate" class="js-trigger">
-					<span></span>Scroll</a>
+					<span></span>Scroll
+				</a>
 			</div>
 		</div>
 	</header>
@@ -37,19 +44,28 @@
 		<div class="flex lg:flex-row md:flex-row flex-col justify-around w-3/4">
 			<div class="border rounded shadow-1 hvr-underline-from-left py-4">
 				<a href="#" class="no-underline">
-					<img src="{{ asset('storage/assets/svg/icons/ic_salario.svg') }}" alt="" class="w-3/4 -mb-6">
+					<img src="{{ asset('storage/assets/svg/icons/ic_salario.svg') }}" 
+					     alt="Manos sosteniendo dinero" 
+					 	 title="El dinero en tus manos"
+						 class="w-3/4 -mb-6">
 				</a>
                 <h4 class="text-2xl font-normal text-blue">Salario</h4>
 			</div>
 			<div class="border rounded shadow-1 hvr-underline-from-center py-4">
 				<a href="#">
-					<img src="{{ asset('storage/assets/svg/icons/ic_vacaciones.svg') }}" alt="" class="w-3/4 -mb-6">
+					<img src="{{ asset('storage/assets/svg/icons/ic_vacaciones.svg') }}" 
+					     alt="Sombrilla bajo el sol." 
+					 	 title="Vacación en la playa bajo el sol." 
+						 class="w-3/4 -mb-6">
 				</a>
                 <h4 class="text-2xl font-normal text-blue">Vacación</h4>
 			</div>
 			<div class="border rounded shadow-1 hvr-underline-from-right py-4">
 				<a href="#">
-					<img src="{{ asset('storage/assets/svg/icons/ic_aguinaldo.svg') }}" alt="" class="w-3/4 -mb-6">
+					<img src="{{ asset('storage/assets/svg/icons/ic_aguinaldo.svg') }}" 
+					     alt="Arbol de Navidad"
+						 title="En Navidad con tu aguinaldo." 
+						 class="w-3/4 -mb-6">
 				</a>
                 <h4 class="text-2xl font-normal text-blue">Aguinaldo</h4>
 			</div>
@@ -58,7 +74,9 @@
 	<section class="flex flex-col lg:flex-row flex-wrap justify-center mt-20">
 		<div class="flex flex-col lg:flex-row justify-center items-center lg:w-3/4 lg:mb-20 mb-10 mx-8">
 			<div class="lg:w-3/5 hidden lg:inline">
-				<img src="{{ asset('storage/assets/svg/illustrations/salario.svg') }}" alt="">
+				<img src="{{ asset('storage/assets/svg/illustrations/salario.svg') }}" 
+				     alt="Manos sosteniendo dinero" 
+					 title="El dinero en tus manos">
 			</div>
 			<div class="lg:w-2/5">
 				<h3 class="lg:text-6xl text-4xl text-grey mb-6">Calculá tu Salario</h3>
@@ -73,12 +91,16 @@
 				<a href="" class="no-underline py-2 px-12 font-bold rounded-full text-blue-dark bg-transparent mb-12 hover:bg-blue-dark hover:text-white border-blue-dark border-2 lg:inline-block block text-center">Leer más</a>
 			</div>
 			<div class="lg:w-3/5 hidden lg:inline">
-				<img src="{{ asset('storage/assets/svg/illustrations/vacaciones.svg') }}" alt="">
+				<img src="{{ asset('storage/assets/svg/illustrations/vacaciones.svg') }}" 
+				     alt="Sombrilla bajo el sol." 
+					 title="Vacación en la playa bajo el sol.">
 			</div>
 		</div>
 		<div class="flex justify-center items-center lg:w-3/4 lg:mb-20 mb-10 mx-8">
 			<div class="lg:w-3/5 hidden lg:inline">
-				<img src="{{ asset('storage/assets/svg/illustrations/aguinaldo.svg') }}" alt="">
+				<img src="{{ asset('storage/assets/svg/illustrations/aguinaldo.svg') }}" 
+				     alt="Arbol de Navidad" 
+					 title="En Navidad con tu aguinaldo.">
 			</div>
 			<div class="lg:w-2/5">
 				<h3 class="lg:text-6xl text-4xl text-grey mb-6">Calculá tu Aguinaldo</h3>
@@ -98,19 +120,28 @@
 				</p>
 			</div>
 			<div class="lg:w-1/3 text-center mb-6">
-				<img src="{{ asset('storage/assets/svg/logo_white.svg') }}" alt="Logo" class="h-11 w-40">
+				<img src="{{ asset('storage/assets/svg/logo_white.svg') }}" alt="Logo SalarioSV" title="Logo SalarioSV" class="h-11 w-40">
 			</div>
 			<div class="lg:w-1/3 flex justify-end">
 				<a href="" class="no-underline">
-					<img src="{{ asset('storage/assets/svg/icons/ic_facebook.svg') }}" alt="Logo Facebook" class="h-8 w-12">
+					<img src="{{ asset('storage/assets/svg/icons/ic_facebook.svg') }}" 
+						 alt="Compartir por Facebook" 
+						 title="Icono de Facebook" 
+						 class="h-8 w-12">
 				</a>
 				<a href="" class="no-underline">
-					<img src="{{ asset('storage/assets/svg/icons/ic_twitter.svg') }}" alt="Logo Twitter" class="h-8 w-12">
+					<img src="{{ asset('storage/assets/svg/icons/ic_twitter.svg') }}" 
+					     alt="Cokpartir por Twitter" 
+						 title="Icono de Twitter" 
+						 class="h-8 w-12">
 				</a>
 			</div>
 		</div>
 		<a href="#" class="float flex justify-center items-center">
-			<img src="{{ asset('storage/assets/svg/icons/ic_arrow.svg') }}" alt="Logo Twitter" class="h-4 w-8">
+			<img src="{{ asset('storage/assets/svg/icons/ic_arrow.svg') }}" 
+			     alt="Regresar a Inicio" 
+				 title="Icono para Regresar a Inicio" 
+				 class="h-4 w-8">
 		</a>
 	</footer>	
 </div>
