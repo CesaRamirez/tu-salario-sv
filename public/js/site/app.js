40e3beebca8a92f7662b29dbfce20434f770941d
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2018-01-26 14:20:07 -0600

    :package: update cors

--- public/js/site/app.js
+++ public/js/site/app.js
@@ -1,1 +1,277 @@
-webpackJsonp([2],{1:function(t,e,n){t.exports=n("ZYXn")},ZYXn:function(t,e,n){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var o=n("I3G/"),i=n.n(o),r=n("M4fF"),a=n.n(r),u=n("mtWM"),c=n.n(u),s=n("e2/S"),d=n.n(s),f=n("A5qe"),l=n.n(f);window.Vue=i.a,window.moveTo=d.a,window._=a.a,window.axios=c.a,window.anime=l.a,window.axios.defaults.headers.common["X-Requested-With"]="XMLHttpRequest";var w=document.head.querySelector('meta[name="csrf-token"]');w&&(window.axios.defaults.headers.common["X-CSRF-TOKEN"]=w.content),new window.Vue({el:"#site"})},"e2/S":function(t,e,n){"use strict";var o=function(){var t={tolerance:0,duration:800,easing:"easeOutQuart",callback:function(){}};function e(t,e,n,o){return t/=o,-n*(--t*t*t*t-1)+e}function n(t,e){var n={};return Object.keys(t).forEach(function(e){n[e]=t[e]}),Object.keys(e).forEach(function(t){n[t]=e[t]}),n}function o(){var o=arguments.length>0&&void 0!==arguments[0]?arguments[0]:{},i=arguments.length>1&&void 0!==arguments[1]?arguments[1]:{};this.options=n(t,o),this.easeFunctions=n({easeOutQuart:e},i)}o.prototype.registerTrigger=function(t,e){var o=this;if(t){var i=t.getAttribute("href")||t.getAttribute("data-target"),r=i&&"#"!==i?document.getElementById(i.substring(1)):document.body,a=n(this.options,function(t,e){var n={};return Object.keys(e).forEach(function(e){var o,i=t.getAttribute("data-mt-"+(o=e,o.replace(/([A-Z])/g,function(t){return"-"+t.toLowerCase()})));i&&(n[e]=isNaN(i)?i:parseInt(i,10))}),n}(t,this.options));"function"==typeof e&&(a.callback=e);var u=function(t){t.preventDefault(),o.move(r,a)};return t.addEventListener("click",u,!1),function(){return t.removeEventListener("click",u,!1)}}},o.prototype.move=function(t){var e=this,o=arguments.length>1&&void 0!==arguments[1]?arguments[1]:{};if(0===t||t){o=n(this.options,o);var i="number"==typeof t?t:t.getBoundingClientRect().top,r=window.pageYOffset,a=null,u=void 0;i-=o.tolerance;window.requestAnimationFrame(function n(c){var s=window.pageYOffset;a||(a=c-1);var d=c-a;if(u&&(i>0&&u>s||i<0&&u<s))return o.callback(t);u=s;var f=e.easeFunctions[o.easing](d,r,i,o.duration);window.scroll(0,f),d<o.duration?window.requestAnimationFrame(n):(window.scroll(0,i+r),o.callback(t))})}},o.prototype.addEaseFunction=function(t,e){this.easeFunctions[t]=e};return o}();t.exports=o}},[1]);
+webpackJsonp([2],{
+
+/***/ 213:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(214);
+
+
+/***/ }),
+
+/***/ 214:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__bootstrap__ = __webpack_require__(215);
+
+
+new window.Vue({
+  el: '#site'
+});
+
+// window.anime({
+//   targets: ['line', 'circle', 'polygon', 'polyline', 'path', 'rect'],
+//   strokeDashoffset: [window.anime.setDashoffset, 0],
+//   easing: 'easeInOutSine',
+//   duration: 2500,
+//   direction: 'alternate',
+//   loop: false
+// });
+
+/***/ }),
+
+/***/ 215:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_vue__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_vue___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_vue__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_lodash__ = __webpack_require__(4);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_lodash___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_lodash__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_axios__ = __webpack_require__(10);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_axios___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_axios__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_moveto__ = __webpack_require__(216);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_moveto___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3_moveto__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_animejs__ = __webpack_require__(24);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_animejs___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_4_animejs__);
+
+
+
+
+
+
+window.Vue = __WEBPACK_IMPORTED_MODULE_0_vue___default.a;
+window.moveTo = __WEBPACK_IMPORTED_MODULE_3_moveto___default.a;
+window._ = __WEBPACK_IMPORTED_MODULE_1_lodash___default.a;
+window.axios = __WEBPACK_IMPORTED_MODULE_2_axios___default.a;
+window.anime = __WEBPACK_IMPORTED_MODULE_4_animejs___default.a;
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+var token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+  window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+}
+
+/***/ }),
+
+/***/ 216:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/*!
+ * MoveTo - A lightweight scroll animation javascript library without any dependency.
+ * Version 1.7.1 (22-10-2017 02:28)
+ * Licensed under MIT
+ * Copyright 2017 Hasan Aydoğdu <hsnaydd@gmail.com>
+ */
+
+
+var MoveTo = function () {
+  /**
+                           * Defaults
+                           * @type {object}
+                           */
+  var defaults = {
+    tolerance: 0,
+    duration: 800,
+    easing: 'easeOutQuart',
+    callback: function callback() {} };
+
+
+  /**
+                                         * easeOutQuart Easing Function
+                                         * @param  {number} t - current time
+                                         * @param  {number} b - start value
+                                         * @param  {number} c - change in value
+                                         * @param  {number} d - duration
+                                         * @return {number} - calculated value
+                                         */
+  function easeOutQuart(t, b, c, d) {
+    t /= d;
+    t--;
+    return -c * (t * t * t * t - 1) + b;
+  }
+
+  /**
+     * Merge two object
+     *
+     * @param  {object} obj1
+     * @param  {object} obj2
+     * @return {object} merged object
+     */
+  function mergeObject(obj1, obj2) {
+    var obj3 = {};
+    Object.keys(obj1).forEach(function (propertyName) {
+      obj3[propertyName] = obj1[propertyName];
+    });
+
+    Object.keys(obj2).forEach(function (propertyName) {
+      obj3[propertyName] = obj2[propertyName];
+    });
+    return obj3;
+  };
+
+  /**
+      * Converts camel case to kebab case
+      * @param  {string} val the value to be converted
+      * @return {string} the converted value
+      */
+  function kebabCase(val) {
+    return val.replace(/([A-Z])/g, function ($1) {
+      return '-' + $1.toLowerCase();
+    });
+  };
+
+  /**
+      * MoveTo Constructor
+      * @param {object} options Options
+      * @param {object} easeFunctions Custom ease functions
+      */
+  function MoveTo() {var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};var easeFunctions = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+    this.options = mergeObject(defaults, options);
+    this.easeFunctions = mergeObject({ easeOutQuart: easeOutQuart }, easeFunctions);
+  }
+
+  /**
+     * Register a dom element as trigger
+     * @param  {HTMLElement} dom Dom trigger element
+     * @param  {function} callback Callback function
+     * @return {function|void} unregister function
+     */
+  MoveTo.prototype.registerTrigger = function (dom, callback) {var _this = this;
+    if (!dom) {
+      return;
+    }
+
+    var href = dom.getAttribute('href') || dom.getAttribute('data-target');
+    // The element to be scrolled
+    var target = href && href !== '#' ?
+    document.getElementById(href.substring(1)) :
+    document.body;
+    var options = mergeObject(this.options, _getOptionsFromTriggerDom(dom, this.options));
+
+    if (typeof callback === 'function') {
+      options.callback = callback;
+    }
+
+    var listener = function listener(e) {
+      e.preventDefault();
+      _this.move(target, options);
+    };
+
+    dom.addEventListener('click', listener, false);
+
+    return function () {return dom.removeEventListener('click', listener, false);};
+  };
+
+  /**
+      * Move
+      * Scrolls to given element by using easeOutQuart function
+      * @param  {HTMLElement|number} target Target element to be scrolled or target position
+      * @param  {object} options Custom options
+      */
+  MoveTo.prototype.move = function (target) {var _this2 = this;var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+    if (target !== 0 && !target) {
+      return;
+    }
+
+    options = mergeObject(this.options, options);
+
+    var distance = typeof target === 'number' ? target : target.getBoundingClientRect().top;
+    var from = window.pageYOffset;
+    var startTime = null;
+    var lastPageYOffset = void 0;
+    distance -= options.tolerance;
+
+    // rAF loop
+    var loop = function loop(currentTime) {
+      var currentPageYOffset = window.pageYOffset;
+
+      if (!startTime) {
+        // To starts time from 1, we subtracted 1 from current time
+        // If time starts from 1 The first loop will not do anything,
+        // because easing value will be zero
+        startTime = currentTime - 1;
+      }
+
+      var timeElapsed = currentTime - startTime;
+
+      if (lastPageYOffset) {
+        if (
+        distance > 0 && lastPageYOffset > currentPageYOffset ||
+        distance < 0 && lastPageYOffset < currentPageYOffset)
+        {
+          return options.callback(target);
+        }
+      }
+      lastPageYOffset = currentPageYOffset;
+
+      var val = _this2.easeFunctions[options.easing](
+      timeElapsed, from, distance, options.duration);
+
+
+      window.scroll(0, val);
+
+      if (timeElapsed < options.duration) {
+        window.requestAnimationFrame(loop);
+      } else {
+        window.scroll(0, distance + from);
+        options.callback(target);
+      }
+    };
+
+    window.requestAnimationFrame(loop);
+  };
+
+  /**
+      * Adds custom ease function
+      * @param {string}   name Ease function name
+      * @param {function} fn   Ease function
+      */
+  MoveTo.prototype.addEaseFunction = function (name, fn) {
+    this.easeFunctions[name] = fn;
+  };
+
+  /**
+      * Returns options which created from trigger dom element
+      * @param  {HTMLElement} dom Trigger dom element
+      * @param  {object} options The instance's options
+      * @return {object} The options which created from trigger dom element
+      */
+  function _getOptionsFromTriggerDom(dom, options) {
+    var domOptions = {};
+
+    Object.keys(options).forEach(function (key) {
+      var value = dom.getAttribute('data-mt-' + kebabCase(key));
+      if (value) {
+        domOptions[key] = isNaN(value) ? value : parseInt(value, 10);
+      }
+    });
+    return domOptions;
+  }
+
+  return MoveTo;
+}();
+
+if (true) {
+  module.exports = MoveTo;
+} else {
+  window.MoveTo = MoveTo;
+}
+
+/***/ })
+
+},[213]);
